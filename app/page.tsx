45fac26c87e75f5d25103e4ed64649f973e161c8
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-12-22 08:47:59 +0100

    all of it?

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,93 @@
+'use client'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from "react"
+import { useRandomReveal } from 'react-random-reveal'
+import Link from 'next/link';
+import {Button} from "@nextui-org/react"
+import { RiTeamLine } from "react-icons/ri";
+import { HiOutlineInformationCircle } from "react-icons/hi";
+import { RiMailLine } from "react-icons/ri";
+import { RiDiscordFill } from "react-icons/ri";
+
+
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col">
+
+      <div className='flex flex-col items-center justify-center w-full h-screen bg-stone-900 text-center'>
+          <h1 className="font-bold text-white text-5xl">
+            McGill Machine Learning Reading Group
+          </h1>
+          <p className="text-lg text-white m-5">
+            Weekly meetings to discuss recent research papers in machine learning.
+          </p>
+        
+          <Link href="https://discord.gg/cgWxx94WTg" rel="noopener noreferrer" target="_blank">
+          <Button color="primary" variant="shadow" className="flex items-center">
+            <RiDiscordFill className="text-2xl" /> 
+            <h5 className="text-bold">Join our Discord</h5>
+          </Button>
+</Link>
+
+      </div>
+
+        <div className='flex flex-col items-center mt-5 mb-5'>
+
+          <div className='flex flex-col items-left justify-left w-1/2'>
+
+          <div className='mt-10 mb-5'>
+            <h2 className="flex font-bold text-3xl">
+              <HiOutlineInformationCircle className="mr-2 text-green-500"/>
+              About
+            </h2> 
+            <p className="text-lg mt-4">
+              The McGill Machine Learning Reading Group is a student-run reading group for machine learning at McGill University. 
+            </p>
+            <p className="text-lg mt-2">
+              We meet weekly to discuss recent research papers in machine learning, and to learn about the latest developments in the field. 
+              We welcome students from all backgrounds and levels of experience.
+            </p>
+          </div>
+
+            <div className='mt-10 mb-5'>
+              <h2 className="flex font-bold text-3xl">
+                <RiTeamLine className="mr-2 text-violet-500"/>
+                Team
+              </h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-lg mt-4">
+                <p className="col-span-1">Tommy</p>
+                <p className="col-span-1">Elian</p>
+                <p className="col-span-1">Edmund</p>
+                <p className="col-span-1">Elliot</p>
+                <p className="col-span-1">Roko</p>
+                <p className="col-span-1">Gabrielle</p>
+                <p className="col-span-1">Tianyi</p>
+                <p className="col-span-1">Louis</p>
+                <p className="col-span-1">Anthony</p>
+                <p className="col-span-1">Santosh</p>
+              </div>
+
+
+            </div>
+
+            <div className='mt-10 mb-5'>
+            <h2 className="flex font-bold text-3xl">
+              <RiMailLine className="mr-2 text-orange-500"/>
+              Contact
+            </h2> 
+            <p className="text-lg mt-4">
+            We're a small, bootstrapped, student-run organization. 
+            If you'd like to get involved, please &#8239;
+            <Link href="https://discord.gg/cgWxx94WTg" target="_blank" className='text-blue-500 underline'>join our Discord</Link> and stay tuned for updates.
+            </p>
+          </div>
+            
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
